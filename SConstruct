a7effedb076eba7125f17b4c81de263dcb84838e
CDevv <cdevyt@gmail.com>
2025-08-30 12:54:07 +0300

    first commit

---
+++ SConstruct
@@ -0,0 +1,2 @@
+env = Environment()
+env.Program('main', Glob('*.c'), LIBS=['m', 'GL', 'glfw3'], LIBPATH='./lib', CPPPATH='./include')
---
+++ main.c
@@ -0,0 +1,54 @@
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
+#include <GL/gl.h>
+#include <stdio.h>
+
+void framebuffer_resize_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, 1);
+    }
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW", NULL, NULL);
+    if (!window) {
+        printf("Failed to create GLFW window.\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD. \n");
+        return -1;
+    }
+
+    glViewport(0, 0, 640, 480);
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
